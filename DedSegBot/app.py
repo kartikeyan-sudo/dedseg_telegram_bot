fe0804fbd5150bc0140b4b02fd9dabe7a54986b7
Kartikeyan Dubey <76141663+kartikeyan-sudo@users.noreply.github.com>
2026-05-10 14:00:13 +0530

    Update app.py

--- DedSegBot/app.py
+++ DedSegBot/app.py
@@ -1,40 +1,11 @@
 from fastapi import FastAPI
-from DedSegBot.quiz import send_daily_quiz
-from DedSegBot.facts import send_daily_fact
-from DedSegBot.motivation import send_motivation
 
 app = FastAPI()
+
 
 @app.get("/")
 def home():
 
     return {
         "status": "DedSeg Backend Running"
-    }
-
-@app.get("/run-quiz")
-def run_quiz():
-
-    send_daily_quiz()
-
-    return {
-        "message": "Quiz sent successfully"
-    }
-
-@app.get("/send-fact")
-def send_fact():
-
-    send_daily_fact()
-
-    return {
-        "message": "Fact sent successfully"
-    }
-
-@app.get("/send-motivation")
-def send_motivation_api():
-
-    send_motivation()
-
-    return {
-        "message": "Motivation sent successfully"
     }
